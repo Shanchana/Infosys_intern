50199e267dd7c6ed2ea0929ffbb4c2712666aec9
shanchana <shanchana.s2022ai-ml@sece.ac.in>
2024-04-28 09:52:09 +0530

    new commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Infosys mern stack internship
---
+++ index.js
@@ -0,0 +1,97 @@
+/* console.log("hello");
+var a =2;
+(a==3)? console.log("ok") : console.log("not ok");
+console.log(a == "2"); //type coercion
+console.log(a === "2"); // no type conversion */
+var s = 1;
+/* for (var i =1; i < 5; i++){
+    console.log(z);
+    let z =3;
+    //console.log(i);
+    console.log(z);
+}
+//console.log(i);
+console.log(z); */
+
+
+//console.log(x);
+let x = 10;
+//console.log(x);
+/* var a ="hello";
+console.log(typeof(s));
+console.log(typeof(a));
+
+console.log(`${a} is good`);
+console.log(a + " is good"); */
+
+/* var sym = Symbol('hai'); //unique symbol
+var sym2 = Symbol('hai');
+console.log(sym == sym2);
+
+(0)?console.log("ok"):console.log("false");
+(1)?console.log("true"):console.log("not ok"); */
+
+/* var animal ={ //object
+    name : 'cat',
+    //age : 12,
+    agee : function(age){
+        console.log("msg " + age);
+    }
+}
+
+console.log(animal.age);
+console.log(animal["name"]);
+animal.agee(10);
+
+var cat = new Object();
+var non = new Object();
+horse = non;
+dog = non;
+var cow = new Object();
+console.log(Object.is(horse,dog)); //different object references false
+console.log(JSON.stringify(cow) == JSON.stringify(dog)); //same data type true  */
+
+//array
+ var arr = [1,,"siva",17,'d']
+ let aar = new Array(23,'cat', new Object());
+/*console.log(arr.length);
+
+arr.forEach(function(item){
+    console.log(item);
+});
+for (var i in arr){
+    console.log(i);
+}
+
+for(var i of arr){
+    console.log(i);
+} */
+/* console.log(arr);
+arr.splice(0,2,"karthi"); 
+console.log(arr);
+arr.splice(3); //used for replacement or deletion
+console.log(arr);
+
+console.log(arr.slice(2)); //slicing
+arr.push(1);
+console.log(arr); // add at last
+arr.pop();
+console.log(arr); // remove at last
+arr.unshift(0);
+console.log(arr); //add at front
+arr.shift();
+console.log(arr); //remove at front */
+
+//console.log(typeof(typeof(-10)))
+
+//function
+
+var no = function(){  //function expression
+    console.log("hai");
+}
+no();
+nom();
+function nom(){  //function keyword
+    console.log("hii");
+}
+
